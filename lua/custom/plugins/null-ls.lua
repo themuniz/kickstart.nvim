98453456b8227c403ddf03b4f57fdcc2eb19f1ba
José Muñiz <jose.muniz@cuny.edu>
2023-09-20 15:07:32 -0400

    null-ls config

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -1,7 +1,7 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
+  'jose-elias-alvarez/null-ls.nvim',
   config = function()
-    local null_ls = require("null-ls")
+    local null_ls = require 'null-ls'
     null_ls.setup {
       sources = {
         null_ls.builtins.formatting.prettier,
@@ -9,18 +9,19 @@ return {
         null_ls.builtins.formatting.autopep8,
         null_ls.builtins.formatting.sqlfluff,
         null_ls.builtins.formatting.shfmt,
-        null_ls.builtins.formatting.rustywind,
+        null_ls.builtins.formatting.rustywind.with { filetypes = { 'html', 'jinja', 'htmldjango' } },
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.djlint.with({
-          filetypes = { "html", "jinja" }
-        }),
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.djlint.with {
+          filetypes = { 'html', 'jinja', 'htmldjango' },
+        },
         null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.diagnostics.djlint.with({
-          filetypes = { "html", "jinja" }
-        }),
+        null_ls.builtins.diagnostics.djlint.with {
+          filetypes = { 'html', 'jinja', 'htmldjango' },
+        },
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.diagnostics.shellcheck,
-      }
+      },
     }
   end,
 }
